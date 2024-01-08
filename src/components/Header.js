fc8c01f0aea24271868e7043da589877bf1f7f53
syntaxslayer <hu4lifefllwer@gmail.com>
2024-01-08 22:41:23 +0100

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div>
+            <button>Dashboard</button>
+            <button>Test Button</button>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
